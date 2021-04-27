972d88c872491d6c7d0b2ec51133717ed1d7c210
JunHyeongChae <78126381+JunHyeongChae@users.noreply.github.com>
2021-04-27 15:46:54 +0900

    Add: 초기세팅 완료 수정

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
 import Theme from './styles/theme';
-import GlobalStyle from './styles/reset';
+import GlobalStyle from './styles/GlobalStyle';
 import { ThemeProvider } from 'styled-components';
 
 ReactDOM.render(
